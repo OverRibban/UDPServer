4293905de4b1a028e03acab264703b7cb6c9887b
OverRibban <44964151+OverRibban@users.noreply.github.com>
2020-04-01 12:53:49 +0200

    Add project files.

---
+++ UDPServer/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            UdpClient client = new UdpClient(12345);
+            byte[] inStream = client.Receive(ref clientEndPoint);
+            tBMessage.Text = Encoding.Unicode.GetString(inStream);
+        }
+    }
+}
